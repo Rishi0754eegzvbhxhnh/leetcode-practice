4caff84b4411b8a3e2924de867da90b5edb3f5c0
KumarRishi <178941557+Rishi0754eegzvbhxhnh@users.noreply.github.com>
2026-08-20 23:33:05 +0530

    Time: 2 ms (6.94%) | Memory: 19.3 MB (52.36%) - LeetSync

---
+++ 68-text-justification/text-justification.py
@@ -0,0 +1,17 @@
+class Solution:
+    def fullJustify(self, words, maxWidth):
+        res, line, length = [], [], 0
+
+        for w in words:
+            if length + len(w) + len(line) > maxWidth:
+                # Distribute spaces
+                for i in range(maxWidth - length):
+                    line[i % (len(line) - 1 or 1)] += ' '
+                res.append(''.join(line))
+                line, length = [], 0
+            line.append(w)
+            length += len(w)
+
+        # Last line: left-justified
+        res.append(' '.join(line).ljust(maxWidth))
+        return res
