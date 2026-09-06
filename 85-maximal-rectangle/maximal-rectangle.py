1a4b41497fcce3ff250e5eb4900af99c6bd72b5f
KumarRishi <178941557+Rishi0754eegzvbhxhnh@users.noreply.github.com>
2026-09-06 22:21:26 +0530

    Time: 32 ms (83.04%) | Memory: 24.5 MB (50.87%) - LeetSync

---
+++ 85-maximal-rectangle/maximal-rectangle.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maximalRectangle(self, matrix):
+        if not matrix:
+            return 0
+        
+        n = len(matrix[0])
+        heights = [0] * (n + 1)  # extra 0 at the end
+        max_area = 0
+        
+        for row in matrix:
+            for i in range(n):
+                # update heights
+                heights[i] = heights[i] + 1 if row[i] == "1" else 0
+            
+            # largest rectangle in histogram
+            stack = [-1]
+            for i in range(n + 1):
+                while heights[i] < heights[stack[-1]]:
+                    h = heights[stack.pop()]
+                    w = i - stack[-1] - 1
+                    max_area = max(max_area, h * w)
+                stack.append(i)
+        
+        return max_area
